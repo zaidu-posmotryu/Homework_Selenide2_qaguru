d6eee7b44427c9837704f131694c9d82af405bdb
daria <cherchimda@gmail.com>
2023-01-01 18:13:09 +0400

    first commit

---
+++ src/test/java/Selenide2Tests.java
@@ -0,0 +1,56 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Selenide2Tests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    @DisplayName("hover на странице Github")
+    void hoverOnGithub() {
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $$(".HeaderMenu-dropdown-link").findBy(text("Enterprise")).click();
+        $(".h1-mktg.mb-3.color-fg-default").shouldHave(text("Build like the best"));
+    }
+
+    @Test
+    @DisplayName("Drag and Drop работает")
+    void dragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));;
+    }
+
+    @Test
+    @Disabled("это не работает")
+    void anotherDragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().dragAndDrop($("#column-a"), $("#column-b")).perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    @Disabled("это тоже не работает")
+    void offsetDragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        SelenideElement element = $("#column-a");
+        actions().dragAndDropBy(element, 200, 0).perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
